ceaa014f367a56aa66ea93c1241b18fd3338f962
joaovictorgit <victoralmeida.0216@gmail.com>
2023-03-02 20:57:24 -0300

    Use swagger in application

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -5,12 +5,87 @@ const userController = new UserController();
 const userExpress = require("express");
 const userRoute = userExpress.Router();
 
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     tags:
+ *     - Usuários
+ *     summary: Cadastra um novo usuário
+ *     description: ""
+ *     parameters:
+ *     - in: body
+ *       name: body
+ *       description: O "objeto usuário" a ser adicionado ao banco de dados
+ *       required: true
+ *       schema:
+ *         type: object
+ *         required:
+ *         - id_user
+ *         - name_user
+ *         - password
+ *         properties:
+ *           id_user:
+ *             type: serial
+ *             description: ID serial gerado automaticamente pelo banco de dados
+ *           name_user:
+ *             type: string
+ *             description: Email do usuário
+ *           email:
+ *             type: string
+ *             description: Email do usuário
+ *           password:
+ *             type: string
+ *             description: Senha do usuário
+ *         responses:
+ *           200:
+ *             description: Usuário cadastrado com sucesso
+ *           400:
+ *             description: Bad Request
+ *           500:
+ *             description: Internal Server Error
+ */
+
 userRoute.post(
   "/",
   (request: Request, response: Response, next: NextFunction) => {
     return userController.createUser(request, response);
   }
 );
+
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     tags:
+ *     - Usuários
+ *     summary: Faz o login do usuário
+ *     description: ""
+ *     parameters:
+ *     - in: body
+ *       name: body
+ *       description: Faz o login do usuário e gera um token
+ *       required: true
+ *       schema:
+ *         type: object
+ *         required:
+ *         - email
+ *         - senha
+ *         properties:
+ *           email:
+ *             type: string
+ *             description: Email do usuário cadastrado no banco de dados
+ *           senha:
+ *             type: string
+ *             description: Senha do usuário cadastrado no banco de dados
+ *         responses:
+ *           200:
+ *             description: OK
+ *           400:
+ *             description: Informações Inválidas
+ *           500:
+ *             description: Internal Server Error
+ */
 
 userRoute.post(
   "/login",
@@ -19,6 +94,21 @@ userRoute.post(
   }
 );
 
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     tags:
+ *     - Usuários
+ *     summary: Mostra todos os usuários cadastrados
+ *     description: ""
+ *     parameters: ""
+ *     responses:
+ *       200:
+ *         description: OK
+ *       500:
+ *         description: Internal Server Error
+ */
 userRoute.get(
   "/",
   (request: Request, response: Response, next: NextFunction) => {
@@ -26,6 +116,28 @@ userRoute.get(
   }
 );
 
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     tags:
+ *     - Usuários
+ *     summary: Mostra um usuário especificado pelo seu id
+ *     description: ""
+ *     parameters:
+ *     - name: id_usuario
+ *       in: path
+ *       required: true
+ *       description: O id do usuário a ser pesquisado
+ *       type: integer
+ *     responses:
+ *       200:
+ *         description: OK
+ *       404:
+ *         description: Usuário não encontrado
+ *       500:
+ *         description: Internal Server Error
+ */
 userRoute.get(
   "/:id",
   (request: Request, response: Response, next: NextFunction) => {
@@ -34,6 +146,33 @@ userRoute.get(
   }
 );
 
+/**
+ * @swagger
+ * /users/{id}:
+ *   patch:
+ *     tags:
+ *     - Usuários
+ *     summary: Altera um usuário especificado pelo seu id
+ *     description: ""
+ *     parameters:
+ *     - name: id_user
+ *       in: path
+ *       required: true
+ *       description: O id do usuário a ser atualizado
+ *       type: integer
+ *     - body: body
+ *       in: body
+ *       required: true
+ *       description: O json contendo os campos e valores a serem atualizados
+ *     responses:
+ *       200:
+ *         description: OK
+ *       404:
+ *         description: Usuário não encontrado
+ *       500:
+ *         description: Internal Server Error
+ */
+
 userRoute.patch(
   "/:id",
   authentication,
@@ -41,6 +180,29 @@ userRoute.patch(
     return userController.updateUser(request, response);
   }
 );
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   delete:
+ *     tags:
+ *     - Usuários
+ *     summary: Deleta um usuário especificado pelo seu id
+ *     description: ""
+ *     parameters:
+ *     - name: id_user
+ *       in: path
+ *       required: true
+ *       description: O id do usuário a ser deletado
+ *       type: integer
+ *     responses:
+ *       200:
+ *         description: OK
+ *       404:
+ *         description: Usuário não encontrado
+ *       500:
+ *         description: Internal Server Error
+ */
 
 userRoute.delete(
   "/:id",
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import routes from "./routes/routes";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const express = require("express");
 const cors = require("cors");
@@ -6,11 +8,42 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
+const HOST = process.env.HOST;
 
+const swaggerOptions: any = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Forum Api",
+      version: "1.0.0",
+      description: 'Documentação para utilização da API "Forum Api".',
+    },
+    host: `http://localhost:${PORT}`,
+    basepath: "/",
+    tags: [
+      {
+        name: "Usuários",
+        description: "Tudo sobre nossos usuários",
+      },
+      {
+        name: "Categorias",
+        description: "Tudo sobre as categorias",
+      },
+      {
+        name: "Comentários",
+        description: "Tudo sobre os comentários realizados no site",
+      },
+    ],
+    schemes: -"http",
+  },
+  apis: ["./src/**/*-router.ts"],
+};
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json());
 app.use(cors());
 app.use("/", routes);
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });

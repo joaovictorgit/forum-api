e4b4f7a95cacaa169f95eeb50194a381e3c73aca
joaovictorgit <victoralmeida.0216@gmail.com>
2023-03-05 09:19:01 -0300

    Update Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import routes from "./routes/routes";
-import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import swaggerDocumment from "../swagger.json";
 
 const express = require("express");
 const cors = require("cors");
@@ -10,46 +10,11 @@ const app = express();
 const PORT = process.env.PORT ?? 5000;
 const HOST = process.env.HOST;
 
-const swaggerOptions: any = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Forum Api",
-      version: "1.0.0",
-      description: 'Documentação para utilização da API "Forum Api".',
-    },
-    host: `http://localhost:${PORT}`,
-    basepath: "/",
-    tags: [
-      {
-        name: "Usuários",
-        description: "Tudo sobre nossos usuários",
-      },
-      {
-        name: "Categorias",
-        description: "Tudo sobre as categorias",
-      },
-      {
-        name: "Comentários",
-        description: "Tudo sobre os comentários realizados no site",
-      },
-    ],
-    schemes: -"http",
-  },
-  apis: ["./src/**/*-router.ts"],
-};
-const swaggerDocs = swaggerJSDoc(swaggerOptions);
-app.use(
-  "/docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocs, { explorer: true })
-);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocumment));
 app.use(express.json());
 app.use(cors());
-app.use("/", routes);
+app.use("/api", routes);
 
 app.listen(PORT, HOST, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
-
-module.exports = app;

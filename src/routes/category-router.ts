aa4aefe4e4a88cfb8808efe974a605a8990c6381
joaovictorgit <victoralmeida.0216@gmail.com>
2023-03-06 14:39:43 -0300

    Update Swagger

--- src/routes/category-router.ts
+++ src/routes/category-router.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import CategoryController from "../controllers/category-controller";
 import express from "express";
+import authentication from "../middleware/authentication";
 
 const categoryRoute = express.Router();
 const categoryController = new CategoryController();
 
 categoryRoute.post(
   "/",
+  authentication,
   (request: Request, response: Response, next: NextFunction) => {
     return categoryController.createCategory(request, response);
   }
@@ -14,6 +16,7 @@ categoryRoute.post(
 
 categoryRoute.get(
   "/",
+  authentication,
   (request: Request, response: Response, next: NextFunction) => {
     return categoryController.showAllCategories(request, response);
   }
@@ -21,6 +24,7 @@ categoryRoute.get(
 
 categoryRoute.get(
   "/:name",
+  authentication,
   (request: Request, response: Response, next: NextFunction) => {
     const name = request.params.name;
     return categoryController.findCategoryByName(name, response);
@@ -29,6 +33,7 @@ categoryRoute.get(
 
 categoryRoute.patch(
   "/:id",
+  authentication,
   (request: Request, response: Response, next: NextFunction) => {
     const id = parseInt(request.params.id);
     return categoryController.updateCategory(id, request, response);
--- src/routes/comment-router.ts
+++ src/routes/comment-router.ts
@@ -1,35 +1,60 @@
 import express, { Request, Response } from "express";
+import authentication from "../middleware/authentication";
 import CommentController from "../controllers/comment-controller";
 
 const commentRoute = express.Router();
 const commentController = new CommentController();
 
-commentRoute.post("/", (request: Request, response: Response) => {
-  return commentController.createComment(request, response);
-});
+commentRoute.post(
+  "/",
+  authentication,
+  (request: Request, response: Response) => {
+    return commentController.createComment(request, response);
+  }
+);
 
-commentRoute.get("/", (request: Request, response: Response) => {
-  return commentController.showAllComments(response);
-});
+commentRoute.get(
+  "/",
+  authentication,
+  (request: Request, response: Response) => {
+    return commentController.showAllComments(response);
+  }
+);
 
-commentRoute.get("/categories/:id", (request: Request, response: Response) => {
-  const id = parseInt(request.params.id);
-  return commentController.showCommentByCategoryId(id, response);
-});
+commentRoute.get(
+  "/categories/:id",
+  authentication,
+  (request: Request, response: Response) => {
+    const id = parseInt(request.params.id);
+    return commentController.showCommentByCategoryId(id, response);
+  }
+);
 
-commentRoute.get("/users/:id", (request: Request, response: Response) => {
-  const id = parseInt(request.params.id);
-  return commentController.showCommentByUserId(id, response);
-});
+commentRoute.get(
+  "/users/:id",
+  authentication,
+  (request: Request, response: Response) => {
+    const id = parseInt(request.params.id);
+    return commentController.showCommentByUserId(id, response);
+  }
+);
 
-commentRoute.patch("/:id", (request: Request, response: Response) => {
-  const id = parseInt(request.params.id);
-  return commentController.updateComment(id, request, response);
-});
+commentRoute.patch(
+  "/:id",
+  authentication,
+  (request: Request, response: Response) => {
+    const id = parseInt(request.params.id);
+    return commentController.updateComment(id, request, response);
+  }
+);
 
-commentRoute.delete("/:id", (request: Request, response: Response) => {
-  const id = parseInt(request.params.id);
-  return commentController.deleteComment(id, response);
-});
+commentRoute.delete(
+  "/:id",
+  authentication,
+  (request: Request, response: Response) => {
+    const id = parseInt(request.params.id);
+    return commentController.deleteComment(id, response);
+  }
+);
 
 export default commentRoute;
